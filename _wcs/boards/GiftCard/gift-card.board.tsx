7f5454559c200448dd2d35f4b6e7844f84045e17
Iurii Kozhan <iuriiko@wix.com>
2022-03-28 15:07:56 +0200

    D: Add a component

--- _wcs/boards/GiftCard/gift-card.board.tsx
+++ _wcs/boards/GiftCard/gift-card.board.tsx
@@ -3,13 +3,15 @@ import { createBoard } from '@wixc3/react-board';
 import { GiftCard } from '../../../src/components/gift-card/gift-card';
 import photoURL from '../../../src/components/gift-card/assets/img-2.jpg';
 import styles from './gift-card.board.module.scss';
+import { MiniCards } from '../../../src/components/mini-cards/mini-cards';
 
 export default createBoard({
     name: 'Example of a gift card panel',
     Board: () => <div className={styles.grid2col}>
-                    <GiftCard photoURL={photoURL}/>
-                    <GiftCard/>
-                </div>,
+        <GiftCard photoURL={photoURL} />
+        <GiftCard />
+        <GiftCard />
+    </div>,
     environmentProps: {
         windowHeight: 700,
         canvasHeight: 600
